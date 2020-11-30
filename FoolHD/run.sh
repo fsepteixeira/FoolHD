cfb0ff4cfab66262945d3e485551ea345b3d8493
fsept11 <57730275+fsept11@users.noreply.github.com>
2020-11-30 15:25:59 +0000

    Update run.sh

--- FoolHD/run.sh
+++ FoolHD/run.sh
@@ -1,1 +1,1 @@
-python3 main.py -configuration conf/parameters_untargted.conf -train -task untargeted --start=0 --end=-1
+python3 main.py -configuration conf/parameters_untargeted.conf -train -task untargeted --start=0 --end=-1
